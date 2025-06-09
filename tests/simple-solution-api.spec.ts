88aa012d2402bea7c069eec826fc14b9cfef93c4
Serx <anissimov92@gmail.com>
2025-06-09 20:31:17 +0300

    Adding negative scenario to order

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -4,12 +4,22 @@ import { StatusCodes } from 'http-status-codes'
 
 test('get order with correct id should receive code 200', async ({ request }) => {
   // Build and send a GET request to the server
-  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/1')
+  const apiResponse = await request.get('https://backend.tallinn-learning.ee/test-orders/1')
   // Log the response status, body and headers
-  console.log('response body:', await response.json())
-  console.log('response headers:', response.headers())
+  console.log('response body:', await apiResponse.json())
+  console.log('response headers:', apiResponse.headers())
   // Check if the response status is 200
-  expect(response.status()).toBe(200)
+  expect(apiResponse.status()).toBe(200)
+})
+
+test('request with  incorrect id should receive code 400', async ({ request }) => {
+  // Build and send a GET request to the server
+  const apiResponse = await request.get('https://backend.tallinn-learning.ee/test-orders/11')
+  // Log the response status, body and headers
+  console.log('response body:', await apiResponse.json())
+  console.log('response headers:', apiResponse.headers())
+  // Check if the response status is 400
+  expect(apiResponse.status()).toBe(400)
 })
 
 test('post order with correct data should receive code 201', async ({ request }) => {
